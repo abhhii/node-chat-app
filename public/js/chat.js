bf91438923a9a96c12e7caad9d511c541b8548d3
Abhishek <abhisheksinghal7599@gmail.com>
2018-11-23 12:32:04 +0530

    Send message to only people in the same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -43,7 +43,7 @@ socket.on('updateUserList', function (users) {
     users.forEach(function (user) {
         ol.append(jQuery('<li></li>').text(user));
     });
-    jQuery('#users').html(ol);
+    jQuery('#users').html(ol); 
 });
 
 socket.on('newMessage', function(message) {
@@ -83,7 +83,6 @@ jQuery('#message-form').on('submit', function(e) {
     var messageTextBox = jQuery('[name=message]');
 
     socket.emit('createMessage', {
-        from: 'User',
         text: messageTextBox.val()
     }, function() {
         messageTextBox.val('');
